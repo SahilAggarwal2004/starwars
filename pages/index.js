ede6b504eff3160fbd48da3abbae90866f6ee74b
Sahil <aggarwalsahil2004@gmail.com>
2022-10-16 22:21:00 +0530

    commit

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,18 @@
 import Image from 'next/image'
-import React, { useEffect } from 'react'
+import React, { useCallback, useEffect } from 'react'
+import Particles from 'react-tsparticles'
+import { loadFull } from 'tsparticles'
 import { useGameContext } from '../contexts/ContextProvider'
 
 export default function Home() {
   const { router, enterFullscreen } = useGameContext()
+
+  const particlesInit = useCallback(async engine => {
+    // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+    // starting from v2 you can add only the features you need reducing the bundle size
+    await loadFull(engine);
+  }, []);
 
   const handlePlay = event => {
     router.push(`/team-selection?mode=${event.target.getAttribute('mode')}`)
@@ -12,8 +21,50 @@ export default function Home() {
 
   useEffect(() => { sessionStorage.removeItem('mode') }, [])
 
+
+
   return <>
-    <Image alt='Star Wars' src='/images/bg.webp' layout='fill' quality={25} />
+    <Particles
+      id="tsparticles"
+      init={particlesInit}
+      options={{
+        background: { color: { value: "#000000" } },
+        interactivity: {
+          events: {
+            onClick: {
+              enable: true,
+              mode: "push",
+            },
+            resize: true
+          }
+        },
+        particles: {
+          color: { value: "#ffffff" },
+          collisions: { enable: true },
+          move: {
+            directions: "none",
+            enable: true,
+            outModes: { default: "bounce" },
+            random: false,
+            speed: 1,
+            straight: false,
+          },
+          number: {
+            density: {
+              enable: true,
+              area: 800,
+            },
+            value: 80,
+          },
+          opacity: { value: 1 },
+          shape: { type: "circle" },
+          size: { value: { min: 1, max: 3 } }
+        },
+        detectRetina: true
+      }}
+    />
+    <Image alt='Star Wars' src='/images/bg.webp' layout='fill' quality={100} />
+    <div className='fixed bg-black inset-0 -z-10' />
     <div className='fixed bottom-8 x-center space-y-3 w-full text-center px-5'>
       <button mode='computer' className='main-button' onClick={handlePlay}>Play vs Computer</button>
       <button mode='player' className='main-button' onClick={handlePlay}>Play vs Player</button>
