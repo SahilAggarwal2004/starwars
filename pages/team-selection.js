d15dadae7e6435b32588fbff9097d1413b5611fd
Sahil Aggarwal <aggarwalsahil2004@gmail.com>
2023-01-04 13:55:09 +0530

    commit

--- pages/team-selection.js
+++ pages/team-selection.js
@@ -50,7 +50,7 @@ export default function TeamSelection({ router, mode }) {
     }
 
     return <>
-        <span className='main-heading x-center top-32'>Select {count < 2 ? 'leader' : 'player'} for Team {currentTeam}</span>
+        <span className='main-heading center -translate-y-[calc(3vw+0.5rem+50%)]'>Select {count < 2 ? 'leader' : 'player'} for Team {currentTeam}</span>
         <div className='grid grid-cols-12 fixed x-center bottom-4 gap-x-2.5 min-w-max'>
             {players.map(player => <div className='relative w-[6vw] aspect-square flex justify-center hover:border-2 hover:outline border-transparent rounded-sm' key={player.name} onPointerEnter={() => setHoverPlayer(player)} onPointerLeave={() => setHoverPlayer()} onClick={() => addPlayer(player)} onContextMenu={event => event.preventDefault()}>
                 <img src={`/images/players/${player.name}.webp`} alt={player.name} width='120' className='rounded-sm aspect-square' />
