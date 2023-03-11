83217a565fc671ba31c25537bec201a79a1b06aa
Sahil Aggarwal <aggarwalsahil2004@gmail.com>
2023-03-11 12:50:14 +0530

    experimental commit

--- next.config.js
+++ next.config.js
@@ -69,7 +69,7 @@ const withPWA = require('next-pwa')({
         },
         {
             urlPattern: /\.(?:js)$/i,
-            handler: 'CacheFirst',
+            handler: 'StaleWhileRevalidate',
             options: { cacheName: 'static-js-assets' }
         },
         {
