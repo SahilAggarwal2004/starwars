f1adfb50988e7c9817c13b01bb814079e0e94e04
Sahil Aggarwal <aggarwalsahil2004@gmail.com>
2023-04-08 19:12:46 +0530

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,13 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    screens: {
+      xs: '320px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+    },
     extend: {
       animation: {
         'spin-fast': 'spin 0.6s ease infinite'
