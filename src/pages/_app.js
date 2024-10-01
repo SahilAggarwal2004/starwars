3aa05949fb2112e55fc71f72b4d90a32a31f4fbe
SahilAggarwal2004 <aggarwalsahil2004@gmail.com>
2024-10-01 22:06:21 +0530

    [chore]: adjust dependencies

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -57,8 +57,8 @@ function MyApp({ Component, pageProps }) {
 
         <meta httpEquiv="Content-Security-Policy" content="default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; connect-src *; worker-src 'self' blob:" />
 
+        <meta name="mobile-web-app-capable" content="yes" />
         <link rel="apple-touch-icon" href="icons/apple-icon-180.png" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
         <link rel="apple-touch-startup-image" href="icons/apple-splash-2048-2732.jpg" media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)" />
         <link rel="apple-touch-startup-image" href="icons/apple-splash-2732-2048.jpg" media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)" />
         <link
