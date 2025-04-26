ddbcf82a55b768956431b5090e74d02e921f2496
SahilAggarwal2004 <aggarwalsahil2004@gmail.com>
2025-04-26 23:38:23 +0530

    [chore]: update dependencies

---
+++ eslint.config.mjs
@@ -0,0 +1,7 @@
+import { FlatCompat } from "@eslint/eslintrc";
+
+const compat = new FlatCompat({ baseDirectory: import.meta.dirname });
+
+const eslintConfig = [...compat.config({ extends: ["next/core-web-vitals"] })];
+
+export default eslintConfig;
